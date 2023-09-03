fed2713f7ad546ee54d458d775e7f1bb49a5b915
Joaquin Botteri <joaquinbotteri3@gmail.com>
2023-09-03 20:03:53 -0300

    inicio proyecto

---
+++ js/product_category.js
@@ -0,0 +1,32 @@
+const product_category = document.getElementById('product-container');
+
+const categorias=[
+    camperas={
+        img:"https://media.revistagq.com/photos/633190d5a323b47f9b54000d/4:3/w_3295,h_2471,c_limit/1324252660",
+        name:"Camperas",
+        url:"/"
+    },
+    pantalones={
+        img:"https://i.blogs.es/5ee9b7/tres-formas-de-lucir-los-pantalones-cargo-y-llevar-todo-contigo-esta-temporada/450_1000.webp",
+        name:"Pantalones",
+        url:"/"
+    },
+    remeras={
+        img:"https://urbanmenshops.com/wp-content/uploads/2022/12/DSC00844-348x464.jpg",
+        name:"Remeras",
+        url:"/"
+    }
+];
+
+categorias.forEach(item => {
+    const div=document.createElement('div');
+    div.classList.add("category_card");
+    div.innerHTML=`
+        <a href=${item.url}>
+            <img src=${item.img}>
+            <h3>${item.name}</h3>
+        </a>  
+    `
+    product_category.appendChild(div);
+    console.log(div);
+});
---
+++ js/products.js
@@ -0,0 +1,25 @@
+
+/*const section_producto = document.getElementById('section-producto');
+
+stock.array.forEach(item => {
+    const product=document.createElement('div');
+    if(item.categoria==="campera"){
+        product.classList.add("product-card");
+        product.innerHTML=`
+        <img src=${item.img} class="img-card"/>
+        <div class="content-card">
+            <h2>${item.categoria}</h2>
+            <h3>${item.marca}</h3>
+            <p>$${item.precio}</p>
+            <button class="button-card">Agregar al carrito</button>
+        </div>
+        `
+        section_producto.appendChild(product);
+    }
+});*/
+
+
+// const itemRec= stock.filter(item => item.precio<40000);
+// console.log(itemRec);
+
+//practicar foreach, find y findIndex, filter, some, if, map, set, slice, append, push, pop, fill, lenght, reduce*
