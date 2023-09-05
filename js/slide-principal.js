5ccbe0b52b8ce6d8a2f5ef3aea7a6ba8e4e88a57
Joaquin Botteri <joaquinbotteri3@gmail.com>
2023-09-04 22:33:11 -0300

    slide arrows

--- js/slide-principal.js
+++ js/slide-principal.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
     $('.slide').slick({
         dots: true,
         arrows: true,
-        slidesToShow: 1
+        slidesToShow: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
     });
 });
