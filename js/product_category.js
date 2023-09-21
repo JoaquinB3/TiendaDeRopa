1e926bb1f1c5d616079eb7c0fffc92ad41d7a16e
Joaquin Botteri <joaquinbotteri3@gmail.com>
2023-09-20 21:02:23 -0300

    agregardo de productos

--- js/product_category.js
+++ js/product_category.js
@@ -1,20 +1,20 @@
-const product_category = document.getElementById('product-container');
+const product_category = document.getElementById('productCard-container');
 
 const categorias=[
     camperas={
         img:"https://media.revistagq.com/photos/633190d5a323b47f9b54000d/4:3/w_3295,h_2471,c_limit/1324252660",
         name:"Camperas",
-        url:"/"
+        url:"http:/html/productos.html"
     },
     pantalones={
         img:"https://i.blogs.es/5ee9b7/tres-formas-de-lucir-los-pantalones-cargo-y-llevar-todo-contigo-esta-temporada/450_1000.webp",
         name:"Pantalones",
-        url:"/"
+        url:"http:/html/productos.html"
     },
     remeras={
         img:"https://urbanmenshops.com/wp-content/uploads/2022/12/DSC00844-348x464.jpg",
         name:"Remeras",
-        url:"/"
+        url:"http:/html/productos.html"
     }
 ];
 
@@ -22,11 +22,10 @@ categorias.forEach(item => {
     const div=document.createElement('div');
     div.classList.add("category_card");
     div.innerHTML=`
-        <a href=${item.url}>
+        <a href=${item.url} data-categoria="${item.name}">
             <img src=${item.img}>
             <h3>${item.name}</h3>
         </a>  
     `
     product_category.appendChild(div);
-    console.log(div);
 });
--- js/products.js
+++ js/products.js
@@ -1,25 +1,27 @@
 
-/*const section_producto = document.getElementById('section-producto');
+const container_producto = document.getElementById('container-productos');
 
-stock.array.forEach(item => {
-    const product=document.createElement('div');
-    if(item.categoria==="campera"){
-        product.classList.add("product-card");
-        product.innerHTML=`
-        <img src=${item.img} class="img-card"/>
-        <div class="content-card">
-            <h2>${item.categoria}</h2>
-            <h3>${item.marca}</h3>
-            <p>$${item.precio}</p>
-            <button class="button-card">Agregar al carrito</button>
-        </div>
-        `
-        section_producto.appendChild(product);
-    }
-});*/
+    stock.forEach(item =>{
+            const product=document.createElement('div');
+            product.classList.add("product-card");
+            product.innerHTML=`
+            <img src=${item.img} class="img-card"/>
+            <div class="content-card">
+                <h2>${item.categoria}</h2>
+                <h3>${item.marca}</h3>
+                <p>$${item.precio}</p>
+                <button class="button-card">Agregar al carrito</button>
+            </div>
+            `
+            container_producto.appendChild(product);
+    });
 
 
-// const itemRec= stock.filter(item => item.precio<40000);
+
+
+
+
+// const cateforia_seleccionada = stock.filter(item => item.precio<40000);
 // console.log(itemRec);
 
 //practicar foreach, find y findIndex, filter, some, if, map, set, slice, append, push, pop, fill, lenght, reduce*
