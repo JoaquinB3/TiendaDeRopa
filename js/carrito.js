6386b9a7e9676c125dca3adcd8607407fbc5d642
Joaquin Botteri <joaquinbotteri3@gmail.com>
2024-03-12 20:11:14 -0300

    carrito terminado

--- js/carrito.js
+++ js/carrito.js
@@ -4,7 +4,6 @@ function agregarAlCarrito (productoID) {
     const productoCarrito = stock.find(prod => prod.id === productoID);
     if (productoCarrito) {
         carrito.push (productoCarrito);
-        mostrarCarrito();
     }else{
         console.log ("El producto no existe");
     }
@@ -13,6 +12,7 @@ function agregarAlCarrito (productoID) {
 function eliminarDelCarrito (productoID) {
     const nuevoCarrito = carrito.filter(prod => prod.id !== productoID);
     carrito = nuevoCarrito;
+    
 }
 
 
@@ -27,7 +27,7 @@ function mostrarCarrito(){
                 <div class="containerImg"><img src=${item.img}></div>
                 <h3 class="nombrePCarrito">${item.nombre}</h3>
                 <span>$${item.precio}</span>
-                <a href="#" id="cerrarCarrito"><i class="fa-solid fa-trash"></i></a>
+                <div class="containerTacho"><i class="fa-solid fa-trash tachoProducto"></i></div>
             </div>
         `
         mostrarProdCarrito.appendChild(div); 
@@ -36,21 +36,52 @@ function mostrarCarrito(){
 
 
 const todosLosProductos = document.getElementById('container-productos');
+const totalVenta = document.getElementById("totalVenta");
+const cantCarrito = document.getElementById("cantCarrito")
+let total=0;
+let cantTotal=0;
 
 todosLosProductos.addEventListener("click", (e)=>{
     if(e.target.classList.contains("button-card")){
         const producto = e.target.parentElement;
         const nombreProducto = producto.querySelector(".product-nombre").textContent;
-        const productoStockEncontrado = stock.find(prod => prod.nombre === nombreProducto)
-        
+        const productoStockEncontrado = stock.find(prod => prod.nombre === nombreProducto);
+
         if(productoStockEncontrado){
             const productoId = productoStockEncontrado.id;
+            total += productoStockEncontrado.precio;
+            totalVenta.textContent = total;
+            cantTotal = carrito.length + 1;
+            cantCarrito.textContent = cantTotal;
             agregarAlCarrito(productoId);
+            mostrarCarrito();
             // console.log (carrito);
         }
     }
 });
 
+const todosLosTachos = document.getElementById("productosCarrito");
+
+todosLosTachos.addEventListener("click", (i)=>{
+    if(i.target.classList.contains("tachoProducto")){
+        const producto = i.target.closest(".containerPCarrito");
+        const nombreProducto = producto.querySelector(".nombrePCarrito").textContent;
+        const productoStockEncontrado = stock.find(prod=> prod.nombre === nombreProducto);
+    
+        if(productoStockEncontrado){
+            const productoId = productoStockEncontrado.id;
+            total -= productoStockEncontrado.precio;
+            totalVenta.textContent = total;
+            cantTotal = carrito.length - 1;
+            cantCarrito.textContent = cantTotal;
+            eliminarDelCarrito(productoId);
+            mostrarCarrito();
+            // console.log(carrito);
+        }else{
+            console.log("El producto no se encontro");
+        }
+    }
+});
 
 
 
